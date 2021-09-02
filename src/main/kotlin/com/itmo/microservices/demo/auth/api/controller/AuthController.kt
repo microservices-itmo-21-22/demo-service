ee02fd209bc2a93f79acc111e18baec2e818ee5b
Max <maksim.golish2002@gmail.com>
2021-09-02 19:22:32 +0300

    feat: Add responses for auth api

--- src/main/kotlin/com/itmo/microservices/demo/auth/api/controller/AuthController.kt
+++ src/main/kotlin/com/itmo/microservices/demo/auth/api/controller/AuthController.kt
@@ -4,6 +4,8 @@ import com.itmo.microservices.demo.auth.api.model.AuthenticationRequest
 import com.itmo.microservices.demo.auth.api.model.AuthenticationResult
 import com.itmo.microservices.demo.auth.api.service.AuthService
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,12 +17,26 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(private val authService: AuthService) {
 
     @PostMapping
-    @Operation(summary = "Authenticate")
+    @Operation(
+        summary = "Authenticate",
+        responses = [
+            ApiResponse(description = "OK", responseCode = "200"),
+            ApiResponse(description = "User not found", responseCode = "404"),
+            ApiResponse(description = "Invalid password", responseCode = "403")
+        ]
+    )
     fun authenticate(@RequestBody request: AuthenticationRequest): AuthenticationResult =
             authService.authenticate(request)
 
     @PostMapping("/refresh")
-    @Operation(summary = "Refresh authentication")
+    @Operation(
+        summary = "Refresh authentication",
+        responses = [
+            ApiResponse(description = "OK", responseCode = "200"),
+            ApiResponse(description = "Authentication error", responseCode = "403")
+        ],
+        security = [SecurityRequirement(name = "bearerAuth")]
+    )
     fun refresh(authentication: Authentication): AuthenticationResult =
             authService.refresh(authentication)
 }
