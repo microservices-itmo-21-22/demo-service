05ffd61a1baeacbbb2b617e45f968e49cc0cfcca
Max <maksim.golish2002@gmail.com>
2021-09-09 15:38:45 +0300

    fix: Remove component scan for library

--- src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
+++ src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
@@ -2,10 +2,8 @@ package com.itmo.microservices.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("com.itmo.microservices")
 class DemoServiceApplication
 
 fun main(args: Array<String>) {
