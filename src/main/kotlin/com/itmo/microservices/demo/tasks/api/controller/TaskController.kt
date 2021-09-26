44529e1425a62a1daf19860952ae3cc3a8acdc2f
slavafive <vyacheslavefimov1999@gmail.com>
2021-09-26 18:20:29 +0300

    Comment

--- src/main/kotlin/com/itmo/microservices/demo/tasks/api/controller/TaskController.kt
+++ src/main/kotlin/com/itmo/microservices/demo/tasks/api/controller/TaskController.kt
@@ -11,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
 import java.util.*
+// comment
 
 @RestController
 @RequestMapping("/tasks")
