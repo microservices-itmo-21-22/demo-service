c2790d3105e02317d4f5f62b6faa46e9c337dad3
asukhovitsky <Andrey.Sukhovitsky@infobip.com>
2021-09-27 12:51:38 +0300

    commented out all DB related stuff

--- src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
+++ src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
@@ -1,5 +1,14 @@
 package com.itmo.microservices.demo
 
+import com.itmo.microservices.demo.bombardier.external.ExternalServiceSimulator
+import com.itmo.microservices.demo.bombardier.external.storage.ItemStorage
+import com.itmo.microservices.demo.bombardier.external.storage.OrderStorage
+import com.itmo.microservices.demo.bombardier.external.storage.UserStorage
+import com.itmo.microservices.demo.bombardier.flow.TestController
+import com.itmo.microservices.demo.bombardier.flow.TestParameters
+import com.itmo.microservices.demo.bombardier.flow.UserManagement
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +17,18 @@ class DemoServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoServiceApplication>(*args)
+
+    val externalServiceMock = ExternalServiceSimulator(OrderStorage(), UserStorage(), ItemStorage())
+    val userManagement = UserManagement(externalServiceMock)
+
+    val testApi = TestController(userManagement, externalServiceMock)
+
+    runBlocking {
+        testApi.startTestingForService(TestParameters("test-service", 1, 1, 5))
+        delay(30_000)
+
+        testApi.getTestingFlowForService("test-service").testFlowCoroutine.join()
+
+        testApi.executor.shutdownNow()
+    }
 }
