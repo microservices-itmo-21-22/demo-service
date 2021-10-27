2a414575e36a750db4873d1b0a49806fbf4b7098
neo_nestea <yakova.nastya@yandex.ru>
2021-10-27 18:53:17 +0300

    Deleted Categories package

--- src/main/kotlin/com/itmo/microservices/demo/categories/api/model/CategoryModel.kt
+++
@@ -1,9 +0,0 @@
-package com.itmo.microservices.demo.categories.api.model
-
-import com.fasterxml.jackson.annotation.JsonInclude
-import java.util.*
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class CategoryModel(
-    val id: UUID?,
-    val name: String?)
