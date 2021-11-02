ed0db27d56c08c119fcaebf1bd8593c33ed94e6e
damage <jphandll@gmail.com>
2021-11-02 16:28:56 +0300

    damage
    fixed bug

--- src/test/kotlin/com/itmo/microservices/demo/notifications/NotificationServiceTest.kt
+++ src/test/kotlin/com/itmo/microservices/demo/notifications/NotificationServiceTest.kt
@@ -1,18 +1,19 @@
-package com.itmo.microservices.demo.notifications.api.service
+package com.itmo.microservices.demo.notifications
 
+import com.itmo.microservices.demo.notifications.api.service.NotificationService
 import com.itmo.microservices.demo.payments.api.model.PaymentModel
 import com.itmo.microservices.demo.products.api.model.ProductModel
 import com.itmo.microservices.demo.products.api.model.ProductType
 import com.itmo.microservices.demo.users.api.model.AppUserModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.testng.annotations.Test
+import org.junit.jupiter.api.Test
 import java.util.*
 
 @SpringBootTest
 class NotificationServiceTest {
     @Autowired
-     var notifi:NotificationService ?= null
+     var notifi: NotificationService?= null
 
     @Autowired
     var user: AppUserModel = AppUserModel( username="tq",name="tian",surname="tt",email="123@qq.com",password="123")
