1ea63c438dced407250f9a9e07fa5f01de8c6bc9
neonestea <44156406+neonestea@users.noreply.github.com>
2021-11-03 16:55:58 +0300

    Delete stock directory

--- stock/api/messaging/Events.kt
+++
@@ -1,9 +0,0 @@
-package com.itmo.microservices.demo.stock.api.messaging
-
-import com.itmo.microservices.demo.stock.api.model.StockItemModel
-
-data class StockItemCreatedEvent(val task: StockItemModel)
-
-data class StockItemReservedEvent(val task: StockItemModel)
-
-data class StockItemDeletedEvent(val task: StockItemModel)
