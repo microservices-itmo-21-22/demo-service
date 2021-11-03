17d440b90dcc43b4a776da408d29b101b24a7e92
neonestea <44156406+neonestea@users.noreply.github.com>
2021-11-03 16:57:44 +0300

    Update V1__initDB304.sql

--- src/main/resources/db/migration/V1__initDB304.sql
+++ src/main/resources/db/migration/V1__initDB304.sql
@@ -1,43 +1,48 @@
 CREATE TABLE IF NOT EXISTS stock
 (
-    id              SERIAL PRIMARY KEY,
+    id              INT8 not null,
     name            VARCHAR(255),
     price           decimal(10, 2),
     total_count     VARCHAR(255),
-    reserved_count  VARCHAR(255),https://github.com/JaleChaki/demo-service-1/blob/3044-1/src/main/resources/db/migration/V1__init_DB.sql
-    category        VARCHAR(255)
+    reserved_count  VARCHAR(255),
+    category        VARCHAR(255),
+    primary key (id)
     );
-
+/*
 CREATE TABLE IF NOT EXISTS catalog_item
 (
-    id              SERIAL PRIMARY KEY,
+    id              INT8 not null,
     product_id      INTEGER REFERENCES stock (id),
-    amount          INTEGER(50)
+    amount          INTEGER(50),
+    primary key (id)
     );
 
 CREATE TABLE IF NOT EXISTS shopping_cart
 (
-    id       SERIAL PRIMARY KEY,
-    status   INTEGER(10)
+    id       INT8 not null,
+    status   INTEGER(10),
+    primary key (id)
     );
 
 CREATE TABLE IF NOT EXISTS user
 (
-    id              SERIAL PRIMARY KEY,
+    id              INT8 not null,
     ip_address      INTEGER(10),
     name            VARCHAR(255),
     email           VARCHAR(255),
     phone           INTEGER(50),
-    last_basket_id  INTEGER REFERENCES shopping_cart (id)
+    last_basket_id  INTEGER REFERENCES shopping_cart (id),
+    primary key (id)
     );
 
 CREATE TABLE IF NOT EXISTS order
 (
-    id          SERIAL PRIMARY KEY,
+    id          INT8 not null,
     status      INTEGER(10),
     basket_id   INTEGER REFERENCES shopping_cart (id),
     user_id     INTEGER REFERENCES user (id),
-    date        DATE
+    date        DATE,
+    primary key (id)
     );
 
 CREATE TABLE IF NOT EXISTS basket_catalog_item
@@ -57,4 +62,4 @@ CREATE TABLE IF NOT EXISTS basket_catalog_item
     order_id        INTEGER REFERENCES order (id),
     address         VARCHAR(255),
     date            DATE
-);
+);*/
