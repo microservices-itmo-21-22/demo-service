fbfe1298576c69a190b942ef1d9de0bdf64dcf23
Alexander Syrtsev <alexander.sytrsev@devexpress.com>
2021-11-09 18:53:17 +0300

    fix order scope

--- src/main/kotlin/com/itmo/microservices/demo/orders/impl/repository/PaymentRepository.kt
+++ src/main/kotlin/com/itmo/microservices/demo/orders/impl/repository/PaymentRepository.kt
@@ -2,6 +2,8 @@ package com.itmo.microservices.demo.orders.impl.repository
 
 import com.itmo.microservices.demo.orders.impl.entity.Payment
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface PaymentRepository : JpaRepository<Payment, UUID> {}
