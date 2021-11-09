9be8a0a9fb27e9fff232bae86bfd49bf037949bb
neonestea <44156406+neonestea@users.noreply.github.com>
2021-11-09 22:46:22 +0300

    Update StockItem.kt

--- src/main/kotlin/com/itmo/microservices/demo/stock/impl/entity/StockItem.kt
+++ src/main/kotlin/com/itmo/microservices/demo/stock/impl/entity/StockItem.kt
@@ -5,7 +5,7 @@ import java.util.*
 import javax.persistence.*
 
 @Entity
-@Table(name = "stock304")
+@Table(name = "stock")
 class StockItem {
 
     @Id
@@ -20,7 +20,7 @@ class StockItem {
     constructor()
 
     constructor(id: UUID? = null, name: String? = null, price: Double?,
-    totalCount: Int?, reservedCount: Int?, category: Category) {
+                totalCount: Int?, reservedCount: Int?, category: Category) {
         this.id = id
         this.name = name
         this.price = price
