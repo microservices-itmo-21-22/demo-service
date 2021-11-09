f85f8ae56440d762595c79e3b9f56ec5ab667228
neonestea <44156406+neonestea@users.noreply.github.com>
2021-11-09 11:31:15 +0300

    Update StockItemModel.kt

--- src/main/kotlin/com/itmo/microservices/demo/stock/api/model/StockItemModel.kt
+++ src/main/kotlin/com/itmo/microservices/demo/stock/api/model/StockItemModel.kt
@@ -9,7 +9,6 @@ data class StockItemModel(
     val id: UUID?,
     val name: String?,
     val price: Double?,
-    val externalPrice: Double?,
     val totalCount: Int?,
     val reservedCount: Int?,
     val category: Category = Category.COMMON)
