d096bf42ce0147508ced7244abb7fe0e18c3d50c
Nikita Logvinenko <43111017+Nalgorithm@users.noreply.github.com>
2021-11-28 17:03:06 +0300

    add metrics (#15)
    * add metrics
    
    * fix test counter
    
    * change version
    
    * add quantile
    
    * update version
    
    * update version
    
    * add test duration metrics
    
    * delete unsued metrics

---
+++ src/main/kotlin/com/itmo/microservices/demo/common/metrics/Metrcis.kt
@@ -0,0 +1,71 @@
+package com.itmo.microservices.demo.common.metrics
+
+import com.itmo.microservices.demo.bombardier.stages.TestStage
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Metrics
+import io.micrometer.core.instrument.Timer
+import kotlinx.coroutines.Job
+import java.util.concurrent.Callable
+import java.util.concurrent.TimeUnit
+
+
+class Metrics {
+    private var tags = arrayListOf<String>()
+
+    fun withTags(vararg tags: String): com.itmo.microservices.demo.common.metrics.Metrics {
+        var m = com.itmo.microservices.demo.common.metrics.Metrics()
+        m.tags = this.tags.clone() as ArrayList<String>
+        m.tags.addAll(tags)
+        return m
+    }
+
+    suspend fun stageDurationRecord(stage: TestStage): TestStage.TestContinuationType {
+        val startTime = System.currentTimeMillis()
+        val res = stage.run()
+        val endTime = System.currentTimeMillis()
+        if (res.iSFailState()) {
+            Timer.builder(stageDurationFailName).publishPercentiles(0.95).tags(*this.tags.toTypedArray())
+                .register(Metrics.globalRegistry).record(endTime - startTime, TimeUnit.MILLISECONDS)
+        } else {
+            Timer.builder(stageDurationOkName).publishPercentiles(0.95).tags(*this.tags.toTypedArray())
+                .register(Metrics.globalRegistry).record(endTime - startTime, TimeUnit.MILLISECONDS)
+        }
+
+        return res
+
+    }
+
+    fun testOkDurationRecord(timeMs: Long) {
+        Timer.builder(testDurationOkName).publishPercentiles(0.95).tags(*this.tags.toTypedArray())
+            .register(Metrics.globalRegistry).record(timeMs, TimeUnit.MILLISECONDS)
+    }
+
+    fun testFailDurationRecord(timeMs: Long) {
+        Timer.builder(testDurationFailName).publishPercentiles(0.95).tags(*this.tags.toTypedArray())
+            .register(Metrics.globalRegistry).record(timeMs, TimeUnit.MILLISECONDS)
+    }
+
+
+    fun externalMethodDurationRecord(f: Callable<okhttp3.Response>): okhttp3.Response {
+        val startTime = System.currentTimeMillis()
+        val resp = f.call()
+        val endTime = System.currentTimeMillis()
+
+        Timer.builder("http_external_duration").publishPercentiles(0.95)
+            .tags(*this.tags.toTypedArray(), "code", resp.code().toString())
+            .register(Metrics.globalRegistry).record(endTime - startTime, TimeUnit.MILLISECONDS)
+
+        return resp
+    }
+
+    private val testOkName = "test_counter_ok"
+    private val testFailName = "test_counter_fail"
+    private val stageDurationOkName = "stage_duration_ok"
+    private val stageDurationFailName = "stage_duration_fail"
+    private val testDurationOkName = "test_duration_ok"
+    private val testDurationFailName = "test_duration_fail"
+
+
+    val stageLabel = "stage"
+    val serviceLabel = "service"
+}
