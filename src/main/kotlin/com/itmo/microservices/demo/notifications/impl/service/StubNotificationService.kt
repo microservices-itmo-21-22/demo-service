a48ec03744117fc33180c66980e51ed0109dc3fb
damage <jphandll@gmail.com>
2021-12-07 18:32:43 +0300

    Update StubNotificationService.kt

--- src/main/kotlin/com/itmo/microservices/demo/notifications/impl/service/StubNotificationService.kt
+++ src/main/kotlin/com/itmo/microservices/demo/notifications/impl/service/StubNotificationService.kt
@@ -7,12 +7,21 @@ import com.itmo.microservices.demo.notifications.impl.repository.NotificationUse
 import com.itmo.microservices.demo.payments.api.model.PaymentModel
 import com.itmo.microservices.demo.products.api.model.ProductModel
 import com.itmo.microservices.demo.users.api.model.AppUserModel
+import com.zaxxer.hikari.util.UtilityElf
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.configurationprocessor.json.JSONObject
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
+import java.lang.Exception
+import java.net.ConnectException
 import java.net.URI
 import java.net.http.*
+import java.util.concurrent.*
+import kotlin.concurrent.thread
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.toJavaDuration
@@ -20,23 +29,28 @@ import kotlin.time.toJavaDuration
 
 @Service
 class StubNotificationService(private val userRepository: NotificationUserRepository) : NotificationService {
-    var count =0
+
+    @Autowired
+    var emailSender:SendEmailToUser?=null
+
     companion object {
         val log: Logger = LoggerFactory.getLogger(StubNotificationService::class.java)
     }
 
     override fun processNewUser(user: AppUserModel) {
-        //userRepository.save(modelToEntity(user))
         log.info("User ${user.name}  was created & should be notified (but who cares)")
-        val call = CallExternalSystemForNotification()
-        call.callExternalSystem()
+        emailSender?.sendEmailToUser(user.name)
+
     }
 
     override fun processPayment(payment: PaymentModel) {
-        //send email to user
         when(payment.status){
-            0->log.info("Payment at ${payment.date},user: ${payment.username} successful")
-            1->log.info("Payment at ${payment.date},user: ${payment.username} not successful")
+            0->{
+                log.info("Payment at ${payment.date},user: ${payment.username} successful")
+            }
+            1->{
+                log.info("Payment at ${payment.date},user: ${payment.username} not successful")
+            }
         }
     }
 
@@ -45,106 +59,120 @@ class StubNotificationService(private val userRepository: NotificationUserReposi
         log.info("Product 'ID:${product.id}  ${product.name}' ${product.price} added into database ")
     }
 
-    private fun modelToEntity(user: AppUserModel): NotificationUser = NotificationUser(
-        name = user.name,
-    )
 }
 
-
-class CallExternalSystemForNotification {
-    companion object {
-        val log: Logger = LoggerFactory.getLogger(CallExternalSystemForNotification::class.java)
-    }
-    private val postToken = mapOf("clientSecret" to "7d65037f-e9af-433e-8e3f-a3da77e019b1")
+@Service
+class SendEmailToUser(){
+    private val postToken = mapOf("clientSecret" to "8ddfb4e8-7f83-4c33-b7ac-8504f7c99205")
     private val objectMapper = ObjectMapper()
     private val postBody: String = objectMapper.writeValueAsString(postToken)
     @OptIn(ExperimentalTime::class)
+    private val timeout =Duration.seconds(10).toJavaDuration()
+    val httpClient: HttpClient = HttpClient.newBuilder().build()
 
     private fun getPostHeaders(body:String): HttpRequest {
         return HttpRequest.newBuilder()
             .uri(URI.create("http://77.234.215.138:30027/transactions"))
-            .timeout(Duration.seconds(10).toJavaDuration())
+            .timeout(this.timeout)
             .header("Content-Type","application/json")
             .POST(HttpRequest.BodyPublishers.ofString(body))
             .build()
     }
 
-    private fun processErrorCode(response: HttpResponse<String>){
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(StubNotificationService::class.java)
+    }
+
+    @Autowired
+    private var executorService: ExecutorService? = null
+
+
+    fun  sendEmailToUser(message:String) {
+        try{
+            executorService?.submit(callExternalSystem(message))
+        }catch(e:RejectedExecutionException){
+            log.info(e.message)
+            Thread.sleep(3000)
+            executorService?.submit(callExternalSystem(message))
+        }
+        catch(e2:ConnectException){
+            log.info(e2.message)
+            executorService?.submit(callExternalSystem(message))
+        }
+        catch (e3:HttpTimeoutException){
+            log.info("Exception： HttpTimeoutException")
+            //retry once
+            executorService?.submit(callExternalSystem(message))
+        }
+    }
+
+
+    fun callExternalSystem(message:String): Runnable {
+        return Runnable {
+            try{
+                val response = httpClient.send(getPostHeaders(postBody), HttpResponse.BodyHandlers.ofString())
+                if(response.statusCode()==200){
+                    processSuccessCode(response,message)
+                }
+                else{
+                    // Something is wrong
+                    processErrorCode(response,message)
+                    Thread.sleep(3000)
+                    sendEmailToUser(message)
+                }
+            }catch (e:HttpConnectTimeoutException){
+                //timeout, try again
+                log.info("Request timeout!")
+                sendEmailToUser(message)
+            }
+        }
+    }
+
+
+    fun processErrorCode(response: HttpResponse<String>,  message:String){
         val responseJson = JSONObject(response.body())
         when(response.statusCode()){
             500->{
-                log.info("error with code 500: transaction not started due to server error")
+                log.info("user: ${message},error with code 500: transaction not started due to server error")
             }
             401->{
-                log.info("error with code 401: authentication error when call external system")
+                log.info("user: ${message},error with code 401: authentication error when call external system")
             }
             429->{
-                log.info("error with code 429: number of requests exceeded")
+                log.info("user: ${message},error with code 429: number of requests exceeded")
             }
         }
-        log.info("time: ${responseJson.getLong("timestamp")}  message: ${responseJson.getString("message")}")
+        log.info("user: ${message},time: ${responseJson.getLong("timestamp")}  message: ${responseJson.getString("message")}")
     }
 
-    private fun processSuccessCode(response: HttpResponse<String>){
+    fun processSuccessCode(response: HttpResponse<String>, message:String){
         val responseJson = JSONObject(response.body())
+        log.info("$message  "+responseJson.getString("status"))
         when(responseJson.getString("status")){
             "SUCCESS"->{
-                log.info("Transaction success: id:${responseJson.getString("id")},completedTime${responseJson.getLong("completedTime")}")
+               log.info("user: ${message},Transaction success: id:${responseJson.getString("id")},completedTime${responseJson.getLong("completedTime")}")
                 return
             }
             "FAILURE"->{
                 //retry post request
-                try {
-                    callExternalSystem()
-                }catch (e:HttpTimeoutException){
-                    log.info("Exception： HttpTimeoutException")
-                    //retry once
-                    callExternalSystem()
-                }
+                sendEmailToUser(message)
             }
         }
     }
-
-//
-//    private fun pollExternalSystem(id:String){
-//        val client = HttpClient.newBuilder().build()
-//        val request=HttpRequest.newBuilder()
-//            .uri(URI.create("http://localhost:8080/transactions/${id}"))
-//            .build()
-//        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-//
-//        if(response.statusCode()==200){
-//            processSuccessCode(response)
-//        }
-//        else{
-//            log.info("Error with code 404:")
-//            if(response.statusCode()==404)return
-//            //sleep 5s and then  poll for result
-//            Thread.sleep(5000)
-//            pollExternalSystem(id)
-//        }
-//    }
-
-    fun callExternalSystem(){
-        val client = HttpClient.newBuilder().build()
-        try{
-            val response= client.send(getPostHeaders(postBody), HttpResponse.BodyHandlers.ofString())
-            if(response.statusCode()==200){
-                processSuccessCode(response)
-            }
-            else{
-                // Something is wrong
-                processErrorCode(response)
-                Thread.sleep(5000)
-                callExternalSystem()
-            }
-        }catch (e:HttpConnectTimeoutException){
-            //timeout, try again
-            log.info("Request timeout!")
-            callExternalSystem()
-        }
-
-
-    }
-
 }
+
+
+@Configuration
+class ThreadPoolConfig {
+    fun executorService(): ExecutorService {
+        return ThreadPoolExecutor(
+            9,
+            17,
+            1L,
+            TimeUnit.MINUTES,
+            ArrayBlockingQueue(1024)
+        )
+    }
+}
+
