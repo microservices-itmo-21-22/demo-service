106f3a466f5e374ca01ba59a684000c262758715
damage <jphandll@gmail.com>
2021-12-14 20:22:16 +0300

    Update DeliveryService.kt

--- src/main/kotlin/com/itmo/microservices/demo/delivery/api/service/DeliveryService.kt
+++ src/main/kotlin/com/itmo/microservices/demo/delivery/api/service/DeliveryService.kt
@@ -1,12 +1,13 @@
 package com.itmo.microservices.demo.delivery.api.service
 
+import com.itmo.microservices.demo.delivery.api.model.DeliveryInfoRecordModel
 import com.itmo.microservices.demo.delivery.impl.entity.DeliveryInfoRecord
 import com.itmo.microservices.demo.delivery.impl.service.Timer
 import com.itmo.microservices.demo.order.api.model.OrderDto
 
 interface DeliveryService{
     fun getSlots(number:Int):List<Int>
-    fun getDeliveryHistoryById(transactionId:String):List<DeliveryInfoRecord>
+    fun getDeliveryHistoryById(transactionId:String):List<DeliveryInfoRecordModel>
     fun delivery(order:OrderDto)
     fun delivery(order:OrderDto,times:Int)
 }
