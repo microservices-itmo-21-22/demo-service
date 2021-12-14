1ed0d3fb90cc56b5a8b18685f556968ceb57a286
damage <jphandll@gmail.com>
2021-12-14 20:03:43 +0300

    Update DefaultDeliveryService.kt

--- src/main/kotlin/com/itmo/microservices/demo/delivery/impl/service/DefaultDeliveryService.kt
+++ src/main/kotlin/com/itmo/microservices/demo/delivery/impl/service/DefaultDeliveryService.kt
@@ -32,41 +32,45 @@ import kotlin.time.toJavaDuration
 
 
 @Service
-class Timer(){
-    var time:Int=0
+class Timer {
+    var time: Int = 0
+
     @PostConstruct
-    fun timerStart(){
+    fun timerStart() {
         val executor: ScheduledExecutorService = Executors.newScheduledThreadPool(1)
         executor.scheduleAtFixedRate(
-            {this.time++},
+            { this.time++ },
             0,
             1000,
-            TimeUnit.MILLISECONDS);
+            TimeUnit.MILLISECONDS
+        )
     }
-    fun get_time()=this.time
-}
 
+    fun get_time() = this.time
+}
 
 
 @Suppress("UnstableApiUsage")
 @Service
-class DefaultDeliveryService(private val deliveryInfoRecordRepository: DeliveryInfoRecordRepository,
-                             private val eventBus: EventBus,
-                             private val timer: Timer
-): DeliveryService {
+class DefaultDeliveryService(
+    private val deliveryInfoRecordRepository: DeliveryInfoRecordRepository,
+    private val eventBus: EventBus,
+    private val timer: Timer
+) : DeliveryService {
 
     private val postToken = mapOf("clientSecret" to "8ddfb4e8-7f83-4c33-b7ac-8504f7c99205")
     private val objectMapper = ObjectMapper()
     private val postBody: String = objectMapper.writeValueAsString(postToken)
+
     @OptIn(ExperimentalTime::class)
     private val timeout = Duration.seconds(10).toJavaDuration()
     val httpClient: HttpClient = HttpClient.newBuilder().build()
 
-    private fun getPostHeaders(body:String): HttpRequest {
+    private fun getPostHeaders(body: String): HttpRequest {
         return HttpRequest.newBuilder()
             .uri(URI.create("http://77.234.215.138:30027/transactions"))
             .timeout(this.timeout)
-            .header("Content-Type","application/json")
+            .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString(body))
             .build()
     }
@@ -80,7 +84,7 @@ class DefaultDeliveryService(private val deliveryInfoRecordRepository: DeliveryI
     }
 
     @Autowired
-    var pollingForResult:PollingForResult?=null
+    var pollingForResult: PollingForResult? = null
 
     var countOrdersWaitingForDeliver = AtomicInteger(0)
     //Virtual time
@@ -88,62 +92,72 @@ class DefaultDeliveryService(private val deliveryInfoRecordRepository: DeliveryI
 
     override fun getSlots(number: Int): List<Int> {
         var list = mutableListOf<Int>()
-        var startTime:Int = timer.get_time()+500+3*countOrdersWaitingForDeliver.get()
-        for (i:Int in 1..number){
+        var startTime: Int = timer.get_time() + 500 + 3 * countOrdersWaitingForDeliver.get()
+        for (i: Int in 1..number) {
             list.add(startTime)
-            startTime+=3
+            startTime += 3
         }
         return list.toList()
     }
 
-    override fun getDeliveryHistoryById(transactionId: String) =deliveryInfoRecordRepository.getAllByTransactionId(UUID.fromString(transactionId))
+    override fun getDeliveryHistoryById(transactionId: String) =
+        deliveryInfoRecordRepository.getAllByTransactionId(UUID.fromString(transactionId))
 
-    override fun delivery(order: OrderDto){delivery(order,1)}
+    override fun delivery(order: OrderDto) {
+        delivery(order, 1)
+    }
 
-    override fun delivery(order: OrderDto,times:Int) {
-        if(order.deliveryDuration!! < this.timer.get_time()){
+    override fun delivery(order: OrderDto, times: Int) {
+        if (order.deliveryDuration!! < this.timer.get_time()) {
             val timeStamp = System.currentTimeMillis()
-            deliveryInfoRecordRepository.save(DeliveryInfoRecord(DeliverySubmissionOutcome.EXPIRED,
-                timeStamp,
-                1,
-                timeStamp,
-                order.id!!,
-                timeStamp))
-        }else{
-           try{
-               val response = httpClient.send(getPostHeaders(postBody), HttpResponse.BodyHandlers.ofString())
-               val responseJson = JSONObject(response.body())
-               if(response.statusCode()==200){
-                  val id= responseJson.getString("id")
-                   pollingForResult?.getDeliveryResult(order,responseJson,1)
-               }else{
+            deliveryInfoRecordRepository.save(
+                DeliveryInfoRecord(
+                    DeliverySubmissionOutcome.EXPIRED,
+                    timeStamp,
+                    1,
+                    timeStamp,
+                    order.id!!,
+                    timeStamp
+                )
+            )
+        } else {
+            try {
+                val response = httpClient.send(getPostHeaders(postBody), HttpResponse.BodyHandlers.ofString())
+                val responseJson = JSONObject(response.body())
+                if (response.statusCode() == 200) {
+                    val id = responseJson.getString("id")
+                    pollingForResult?.getDeliveryResult(order, responseJson, 1)
+                } else {
+                    Thread.sleep(3000)
                     delivery(order)
-               }
-           }catch (e: HttpConnectTimeoutException){
-               log.info("Request timeout!")
-               delivery(order)
-           }
+                }
+            } catch (e: HttpConnectTimeoutException) {
+                log.info("Request timeout!")
+                delivery(order)
+            }
         }
     }
 }
 
 
 @Service
-class PollingForResult(private val deliveryInfoRecordRepository: DeliveryInfoRecordRepository,
-                       private val timer: Timer)
-{
+class PollingForResult(
+    private val deliveryInfoRecordRepository: DeliveryInfoRecordRepository,
+    private val timer: Timer
+) {
     private val postToken = mapOf("clientSecret" to "8ddfb4e8-7f83-4c33-b7ac-8504f7c99205")
     private val objectMapper = ObjectMapper()
     private val postBody: String = objectMapper.writeValueAsString(postToken)
+
     @OptIn(ExperimentalTime::class)
-    private val timeout =Duration.seconds(10).toJavaDuration()
+    private val timeout = Duration.seconds(10).toJavaDuration()
     val httpClient: HttpClient = HttpClient.newBuilder().build()
 
-    private fun getGetHeaders(id:String): HttpRequest {
+    private fun getGetHeaders(id: String): HttpRequest {
         return HttpRequest.newBuilder()
             .uri(URI.create("http://77.234.215.138:30027/transactions/$id"))
             .timeout(this.timeout)
-            .header("Content-Type","application/json")
+            .header("Content-Type", "application/json")
             .build()
     }
 
@@ -153,39 +167,55 @@ class PollingForResult(private val deliveryInfoRecordRepository: DeliveryInfoRec
 
     var schedulePool: ScheduledExecutorService = Executors.newScheduledThreadPool(2)
 
-    fun getDeliveryResult(order: OrderDto, responseJson_post:JSONObject, times:Int){
-        if(order.deliveryDuration!! < timer.get_time()){
-            deliveryInfoRecordRepository.save(DeliveryInfoRecord(DeliverySubmissionOutcome.EXPIRED,
-                System.currentTimeMillis(),
-                times,
-                responseJson_post.getLong("submitTime"),
-                order.id!!,
-                responseJson_post.getLong("submitTime")))
+    fun getDeliveryResult(order: OrderDto, responseJson_post: JSONObject, times: Int) {
+        if (order.deliveryDuration!! < timer.get_time()) {
+            deliveryInfoRecordRepository.save(
+                DeliveryInfoRecord(
+                    DeliverySubmissionOutcome.EXPIRED,
+                    System.currentTimeMillis(),
+                    times,
+                    responseJson_post.getLong("submitTime"),
+                    order.id!!,
+                    responseJson_post.getLong("submitTime")
+                )
+            )
         }
-        if(times>3){
-           return
+        if (times > 3) {
+            return
         }
         schedulePool.schedule(
             {
-                val response_poll =httpClient.send(getGetHeaders(responseJson_post.getString("id")),HttpResponse.BodyHandlers.ofString())
+                val response_poll = httpClient.send(
+                    getGetHeaders(responseJson_post.getString("id")),
+                    HttpResponse.BodyHandlers.ofString()
+                )
                 val responseJson_poll = JSONObject(response_poll.body())
-                if(responseJson_poll.getString("status")=="SUCCESS"){
-                    deliveryInfoRecordRepository.save(DeliveryInfoRecord(DeliverySubmissionOutcome.SUCCESS,
-                        responseJson_poll.getLong("delta"),
-                        times,
-                        responseJson_poll.getLong("completedTime"),
-                        order.id!!,
-                        responseJson_poll.getLong("submitTime")))
-                }else{
-                    deliveryInfoRecordRepository.save(DeliveryInfoRecord(DeliverySubmissionOutcome.FAILURE,
-                        responseJson_poll.getLong("delta"),
-                        times,
-                        responseJson_poll.getLong("completedTime"),
-                        order.id!!,
-                        responseJson_poll.getLong("submitTime")))
+                if (responseJson_poll.getString("status") == "SUCCESS") {
+                    deliveryInfoRecordRepository.save(
+                        DeliveryInfoRecord(
+                            DeliverySubmissionOutcome.SUCCESS,
+                            responseJson_poll.getLong("delta"),
+                            times,
+                            responseJson_poll.getLong("completedTime"),
+                            order.id!!,
+                            responseJson_poll.getLong("submitTime")
+                        )
+                    )
+                } else {
+                    deliveryInfoRecordRepository.save(
+                        DeliveryInfoRecord(
+                            DeliverySubmissionOutcome.FAILURE,
+                            responseJson_poll.getLong("delta"),
+                            times,
+                            responseJson_poll.getLong("completedTime"),
+                            order.id!!,
+                            responseJson_poll.getLong("submitTime")
+                        )
+                    )
                 }
 
-            }, ((order.deliveryDuration -timer.get_time())/3).toLong(),TimeUnit.SECONDS)
+            }, ((order.deliveryDuration - timer.get_time()) / 3).toLong(), TimeUnit.SECONDS
+        )
     }
 
 }
