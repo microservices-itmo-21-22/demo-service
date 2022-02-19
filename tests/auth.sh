1b3c58461f3f9de2d4e8c15add92383c43636cfe
Vlad Yandola <vladan2000.9@mail.ru>
2022-02-19 15:49:07 +0300

    Дополнительные тесты

--- tests/auth.sh
+++ tests/auth.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
 url="http://localhost:8080"
 
-curl -X POST "$url/authentication" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"name\":\"$1\",\"password\":\"$2\"}" | jq -r '.accessToken'
+refreshToken=$(curl -sX POST "$url/authentication" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"name\":\"$1\",\"password\":\"$2\"}" | jq -r '.refreshToken')
+curl -sX POST "http://localhost:8080/authentication/refresh" -H  "accept: */*" -H  "Authorization: Bearer $refreshToken" | jq -r '.accessToken'
--- tests/users.sh
+++ tests/users.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
 url="http://localhost:8080"
 
-curl -X POST "$url/users" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"name\":\"$1\",\"password\":\"$2\"}" | jq '.'
-
+curl -sX POST "$url/users" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"name\":\"$1\",\"password\":\"$2\"}" | jq '.'
