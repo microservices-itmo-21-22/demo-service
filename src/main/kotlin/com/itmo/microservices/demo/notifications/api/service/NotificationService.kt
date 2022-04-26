33587d9ea68835df443d82a7783b7a04245a97f0
Владислав <v.pyslar@nyamee.com>
2022-04-26 20:32:29 +0300

    delete packages

--- src/main/kotlin/com/itmo/microservices/demo/notifications/api/service/NotificationService.kt
+++
@@ -1,9 +0,0 @@
-package com.itmo.microservices.demo.notifications.api.service
-
-import com.itmo.microservices.demo.tasks.api.model.TaskModel
-import com.itmo.microservices.demo.users.api.model.AppUserModel
-
-interface NotificationService {
-    fun processNewUser(user: AppUserModel)
-    fun processAssignedTask(task: TaskModel)
-}
