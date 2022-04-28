859688bf84cd0fb561202dbf072693df1fffbfee
Maksim Golish <Maksim.Golish@Dell.com>
2022-04-28 20:06:47 +0300

    chore: Fix model

--- src/main/kotlin/com/itmo/microservices/demo/delivery/api/model/BookingDto.kt
+++ src/main/kotlin/com/itmo/microservices/demo/delivery/api/model/BookingDto.kt
@@ -1,9 +1,8 @@
 package com.itmo.microservices.demo.delivery.api.model
 
-import jdk.management.jfr.SettingDescriptorInfo
 import java.util.*
 
-class BookingDto {
-    var id: UUID = UUID.randomUUID()
-    var failedItems: Set<UUID> = HashSet<UUID>()
-}
+data class BookingDto (
+    var id: UUID,
+    var failedItems: Set<UUID>
+)
