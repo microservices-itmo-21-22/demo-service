198812699d776a7c1f4a7dbe016dabeac9ec941f
kataevrsc <kataevrsc@bitbucket.org>
2022-05-03 21:12:16 +0300

    add order controller draft

---
+++ src/main/kotlin/com/itmo/microservices/demo/order/api/controller/OrderController.kt
@@ -0,0 +1,9 @@
+package com.itmo.microservices.demo.order.api.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/orders")
+class OrderController() {
+}
--- src/main/kotlin/com/itmo/microservices/demo/order/api/model/OrderModel.kt
+++ src/main/kotlin/com/itmo/microservices/demo/order/api/model/OrderModel.kt
@@ -1,14 +1,17 @@
 package com.itmo.microservices.demo.order.api.model
 
 import java.util.*
+import javax.persistence.OneToMany
 
 data class OrderModel(
     val id: UUID,
     val timeCreated: Long,
     val status: OrderStatus,
+    @OneToMany
     var itemsMap: List<ItemMap>,
-    var deliveryDuration: Int?,
-    var paymentHistory: List<PaymentLogRecord>)
+    var deliveryDuration: Int?//,
+    //var paymentHistory: List<PaymentLogRecord>
+    )
 {
     //fun OrderDetails(): OrderDto = OrderDto(timeCreated, status, deliveryDuration)
 }
